d9609c4b4453b1634814b38e6cadd2c54fdace77
Scott Haney <scotthaney323@gmail.com>
2022-09-19 21:05:10 -0400

    Add in a text measuring class and a test for it

---
+++ MatrixLayout.Tests/TextMeasurerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixLayout.Tests
+{
+    public class TextMeasurerTests
+    {
+        [Test]
+        public void RunsWithoutError()
+        {
+            var textMeasurer = new TextMeasurer();
+            textMeasurer.MeasureText("Testing", new System.Drawing.Font("Arial", 12));
+        }
+    }
+}
---
+++ MatrixLayout/TextMeasurer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MatrixLayout
+{
+    public class TextMeasurer : ITextMeasurer
+    {
+        public SizeF MeasureText(string text, Font font)
+        {
+            Bitmap objBitmap = default(Bitmap);
+            Graphics objGraphics = default(Graphics);
+
+            objBitmap = new Bitmap(500, 200);
+            objGraphics = Graphics.FromImage(objBitmap);
+
+            SizeF stringSize = objGraphics.MeasureString(text, font);
+
+            objBitmap.Dispose();
+            objGraphics.Dispose();
+            return stringSize;
+        }
+    }
+
+    public interface ITextMeasurer
+    {
+        SizeF MeasureText(string text, Font font);
+    }
+}
