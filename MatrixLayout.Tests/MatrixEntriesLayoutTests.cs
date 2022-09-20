aab8c57c443f96dacbaf7c945420a5568b0052d0
Scott Haney <scotthaney323@gmail.com>
2022-09-19 20:20:30 -0400

    Add a test for the row gap

--- MatrixLayout.Tests/MatrixEntriesLayoutTests.cs
+++ MatrixLayout.Tests/MatrixEntriesLayoutTests.cs
@@ -32,5 +32,15 @@ namespace MatrixLayout.Tests
             Assert.AreEqual(new RectangleF(10, 10, 35, 80), results.GetEntryBounds(0, 0));
             Assert.AreEqual(new RectangleF(55, 10, 35, 80), results.GetEntryBounds(0, 1));
         }
+
+        [Test]
+        public void TwoByOneMatrixWithOuterPaddingAndRowGapWorksCorrectly()
+        {
+            var layout = new MatrixEntriesLayout(0.10f, 0.10f, 0, 2, 1);
+            var results = layout.GetLayoutResult(new RectangleF(0, 0, 100, 100));
+
+            Assert.AreEqual(new RectangleF(10, 10, 80, 35), results.GetEntryBounds(0, 0));
+            Assert.AreEqual(new RectangleF(10, 55, 80, 35), results.GetEntryBounds(0, 1));
+        }
     }
 }
