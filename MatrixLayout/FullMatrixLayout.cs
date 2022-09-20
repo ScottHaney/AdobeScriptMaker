21edf043c336219ef64da0ab5b9338b56b84eae4
Scott Haney <scotthaney323@gmail.com>
2022-09-19 20:30:16 -0400

    Add a class for laying out a matrix and its brackets

---
+++ MatrixLayout/FullMatrixLayout.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MatrixLayout
+{
+    public class FullMatrixLayout
+    {
+        private readonly MatrixEntriesLayout _entriesLayout;
+        private readonly float _bracketThickness;
+
+        public FullMatrixLayout(MatrixEntriesLayout entriesLayout,
+            float bracketThickness)
+        {
+            _entriesLayout = entriesLayout;
+            _bracketThickness = bracketThickness;
+        }
+
+        public FullMatrixLayoutResult GetLayoutResult(RectangleF availableSpace)
+        {
+            var entriesRect = new RectangleF(availableSpace.Left + _bracketThickness,
+                availableSpace.Top + _bracketThickness,
+                availableSpace.Width - 2 * _bracketThickness,
+                availableSpace.Height - 2 * _bracketThickness);
+
+            var entriesLayoutResult = _entriesLayout.GetLayoutResult(entriesRect);
+            return new FullMatrixLayoutResult(entriesLayoutResult, _bracketThickness);
+        }
+    }
+
+    public class FullMatrixLayoutResult
+    {
+        private readonly MatrixEntriesLayoutResult _entriesResult;
+        private readonly float _bracketThickness;
+
+        public FullMatrixLayoutResult(MatrixEntriesLayoutResult entriesResult,
+            float bracketThickness)
+        {
+            _entriesResult = entriesResult;
+            _bracketThickness= bracketThickness;
+        }
+
+        public RectangleF GetEntryBounds(int rowIndex, int columnIndex)
+        {
+            return _entriesResult.GetEntryBounds(rowIndex, columnIndex);
+        }
+    }
+}
