6f578381473ca822840b46338262671380976b29
Scott Haney <scotthaney323@gmail.com>
2022-09-20 22:32:04 -0400

    Add the start of the code for creating the after effects script for a text layer

--- AdobeScriptMaker.Core/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AdobeScriptMaker.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ AdobeScriptMaker.Core/MatrixScriptCreator.cs
@@ -0,0 +1,35 @@
+﻿using MatrixLayout.ExpressionLayout.LayoutResults;
+using System;
+
+namespace AdobeScriptMaker.Core
+{
+    public class MatrixScriptCreator
+    {
+        public string CreateScript(ILayoutResults layoutResults)
+        {
+            foreach (var result in layoutResults.GetResults())
+            {
+                if (result is TextLayoutResult textLayoutResult)
+                {
+
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private string CreateTextLayer(string source, string value, string fontName, double fontSize)
+        {
+            //https://ae-scripting.docsforadobe.dev/layers/layercollection.html#layercollection-addtext
+            var layer = $"var layer = {source}.layers.addText({value});";
+
+            //https://www.youtube.com/watch?v=6P76aFYmOR8&t=11s
+            var setText = $@"var textDocument = new TextDocument('{value}');
+textDocument.text = '{value}';
+textDocument.font = '{fontName}';
+layer.property('Source Text').setValue(textDocument);";
+
+            return string.Join(Environment.NewLine, layer, setText);
+        }
+    }
+}
