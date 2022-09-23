7d71a97327cc9b628b66f0fb106e16a9dbe6e3e8
Scott Haney <scotthaney323@gmail.com>
2022-09-23 16:45:00 -0400

    Add in input description classes for matrices

---
+++ MatrixLayout/InputDescriptions/MatrixBracketsDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixLayout.InputDescriptions
+{
+    public class MatrixBracketsDescription
+    {
+        public readonly float Thickness;
+        public readonly float PincerWidthPercentage;
+
+        public MatrixBracketsDescription(float thickness, float pincerWidthPercentage)
+        {
+            Thickness = thickness;
+            PincerWidthPercentage = pincerWidthPercentage;
+        }
+    }
+}
---
+++ MatrixLayout/InputDescriptions/TextDisplayDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixLayout.InputDescriptions
+{
+    public class TextDisplayDescription
+    {
+        public readonly string FontName;
+        public readonly int FontSize;
+
+        public TextDisplayDescription(string fontName, int fontSize)
+        {
+            FontName = fontName;
+            FontSize = fontSize;
+        }
+    }
+}
