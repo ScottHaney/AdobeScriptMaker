afce89eef3aac6124de7373fcff6b83a653e31e4
Scott Haney <scotthaney323@gmail.com>
2022-09-23 20:47:08 -0400

    Update the script creation code to include font information

--- AdobeScriptMaker.Core/MatrixScriptCreator.cs
+++ AdobeScriptMaker.Core/MatrixScriptCreator.cs
@@ -8,6 +8,8 @@ namespace AdobeScriptMaker.Core
     {
         public string CreateScript(ILayoutResults layoutResults)
         {
+            var context = new ScriptContext();
+
             var compositionItem = "app.project.activeItem";
 
             var results = new StringBuilder();
@@ -15,21 +17,39 @@ namespace AdobeScriptMaker.Core
             {
                 if (result is MatrixEntryLayoutResult entryResult)
                 {
-                    results.AppendLine(CreateTextLayer(compositionItem, entryResult.Text, "Arial", 12));
+                    results.AppendLine(CreateTextLayer(context, compositionItem, entryResult.Text, "Arial", 12));
                 }
             }
 
             return results.ToString();
         }
 
-        private string CreateTextLayer(string adobeCompositionItem, string value, string fontName, double fontSize)
+        private string CreateTextLayer(ScriptContext context, string adobeCompositionItem, string value, string fontName, double fontSize)
         {
             var lines = new StringBuilder();
 
+            //https://ae-scripting.docsforadobe.dev/other/textdocument.html?highlight=TextDocument#textdocument
+            var textDocVar = context.GetNextAutoVariable();
+            lines.AppendLine(@$"var {textDocVar} = new TextDocument('{value}');
+{textDocVar}.font = 'Arial';
+{textDocVar}.fontSize = 12;
+{textDocVar}.justification = ParagraphJustification.RIGHT_JUSTIFY;");
+
             //https://ae-scripting.docsforadobe.dev/layers/layercollection.html#layercollection-addtext
-            lines.Append($"var layer = {adobeCompositionItem}.layers.addText(new TextDocument('{value}'));");
+            lines.Append($"{adobeCompositionItem}.layers.addText({textDocVar});");
 
             return lines.ToString();
+        }
+    }
+
+    public class ScriptContext
+    {
+        private int _numAutoVariables = 0;
+
+        public string GetNextAutoVariable()
+        {
+            _numAutoVariables++;
+            return $"v{_numAutoVariables}";
         }
     }
 }
