538f795b5f353914e175976ebb4836f1feaed0ad
Scott Haney <scotthaney323@gmail.com>
2022-10-02 10:50:15 -0400

    Start a test for the NumericMultiplierComponent

---
+++ MatrixLayout.Tests/ExpressionLayout/MatrixExpressionLayoutTests.cs
@@ -0,0 +1,28 @@
+﻿using MatrixLayout.ExpressionDecorators;
+using MatrixLayout.ExpressionLayout;
+using MatrixLayout.InputDescriptions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatrixLayout.Tests.ExpressionLayout
+{
+    public class MatrixExpressionLayoutTests
+    {
+        [Test]
+        public void NumericMultiplierComponent_Test()
+        {
+            var layout = new MatrixExpressionLayout(
+                new TextDisplayDescription("Arial", 72),
+                new MatrixLayoutDescription(
+                    new MatrixBracketsDescription(3, 20),
+                    new MatrixInteriorMarginsDescription(0.10f, 0, 0)));
+
+            var result = layout.Layout(new NumericMultiplierComponent(3, new MatrixComponent(3, 1, 1, 1, 1)));
+
+            var innerResults = result.GetResults().ToList();
+        }
+    }
+}
