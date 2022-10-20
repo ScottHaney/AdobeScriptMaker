01a843031ed08c4b8a74019965a5bcde0f00ae65
Scott Haney <scotthaney323@gmail.com>
2022-10-19 20:10:08 -0400

    Add in the start of the direct rendering module

---
+++ DirectRendering/Drawing/AnimatedDrawing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectRendering.Drawing
+{
+    public class AnimatedDrawing
+    {
+        public readonly LineDrawing Drawing;
+        public readonly float DrawingTime;
+
+        public AnimatedDrawing(LineDrawing drawing, float drawingTime)
+        {
+            Drawing = drawing;
+            DrawingTime = drawingTime;
+        }
+    }
+}
---
+++ DirectRendering/Drawing/LineDrawing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace DirectRendering.Drawing
+{
+    public class LineDrawing
+    {
+        public readonly Point Start;
+        public readonly Point End;
+
+        public LineDrawing(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
