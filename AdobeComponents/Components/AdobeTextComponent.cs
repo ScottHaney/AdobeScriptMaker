00edbc13067b688632d693af73a97d808c2e8ba4
Scott Haney <scotthaney323@gmail.com>
2022-12-13 21:02:44 -0500

    Create an AdobeTextComponent

---
+++ AdobeComponents/Components/AdobeTextComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AdobeComponents.Components
+{
+    public class AdobeTextComponent : IAdobeLayerComponent
+    {
+        public readonly string TextValue;
+        public readonly RectangleF Bounds;
+        public readonly AdobeTextSettings TextSettings;
+
+        public AdobeTextComponent(string textValue,
+            RectangleF bounds,
+            AdobeTextSettings textSettings)
+        {
+            TextValue = textValue;
+            Bounds = bounds;
+            TextSettings = textSettings;
+        }
+    }
+
+    public class AdobeTextSettings
+    {
+        public readonly string FontName;
+        public readonly float FontSizeInPixels;
+
+        public AdobeTextSettings(string fontName,
+            float sizeInPoints)
+        {
+            FontName = fontName;
+            FontSizeInPixels = (sizeInPoints * 4) / 3;
+        }
+    }
+}
