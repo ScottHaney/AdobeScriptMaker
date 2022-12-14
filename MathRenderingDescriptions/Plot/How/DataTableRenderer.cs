acb0900ecc451335bd533c9256656b92c9cfc6b5
Scott Haney <scotthaney323@gmail.com>
2022-12-13 20:13:30 -0500

    Start to work on the renderer for the data table

---
+++ MathRenderingDescriptions/Plot/How/DataTableRenderer.cs
@@ -0,0 +1,54 @@
+﻿using MathRenderingDescriptions.Plot.What;
+using MatrixLayout.ExpressionLayout.Matrices;
+using RenderingDescriptions.How;
+using RenderingDescriptions.When;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MatrixLayout.InputDescriptions;
+using System.Linq;
+using MatrixLayout;
+using System.Drawing;
+using AdobeComponents.Components;
+using AdobeComponents.Animation;
+
+namespace MathRenderingDescriptions.Plot.How
+{
+    public class DataTableRenderer : IHowToRender
+    {
+        private readonly DataTableRenderingDescription _description;
+
+        public DataTableRenderer(DataTableRenderingDescription description)
+        {
+            _description = description;
+        }
+
+        public RenderedComponents Render(AbsoluteTiming whenToRender)
+        {
+            var matrixLayout = new SizedToEntriesMatrixEntriesLayout(
+                new MatrixInteriorMarginsDescription(0.1f, 0.1f, 0.1f, 0.1f),
+                _description.Data.NumRows,
+                _description.Data.NumColumns);
+
+            var layoutResult = matrixLayout.GetLayoutResult(new SizedMatrixEntriesLayoutInputParams(
+                new TextMeasurer(),
+                new Font("Tahoma", 50, GraphicsUnit.Pixel),
+                _description.Data.AllDataInMatrixOrder().ToArray()));
+
+            /*var textControls = new List<AdobeTextControl>();
+            for (int row = 0; row < _description.Data.NumRows; row++)
+            {
+                for (int col = 0; col < _description.Data.NumColumns; col++)
+                {
+                    var entryBounds = layoutResult.GetEntryBounds(row, col);
+                    textControls.Add(new AdobeTextControl()
+                    {
+                        Values = new AdobeTextControlValue[] { new AdobeTextControlValue() { Value = "5", Time = 0 } }
+                    });
+                }
+            }*/
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MathRenderingDescriptions/Plot/What/DataTableRenderingDescription.cs
@@ -0,0 +1,43 @@
+﻿using RenderingDescriptions.What;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MathRenderingDescriptions.Plot.What
+{
+    public class DataTableRenderingDescription : IWhatToRender
+    {
+        public readonly DataTableData Data;
+
+        public DataTableRenderingDescription(DataTableData data)
+        {
+            Data = data;
+        }
+    }
+
+    public class DataTableData
+    {
+        public readonly int NumRows;
+        public readonly int NumColumns;
+
+        private readonly List<List<double>> _data;
+
+        public DataTableData(List<List<double>> data)
+        {
+            _data = data;
+            NumRows = data.Count;
+            NumColumns = data.First().Count;
+        }
+
+        public double GetEntry(int row, int column)
+        {
+            return _data[row][column];
+        }
+
+        public IEnumerable<double> AllDataInMatrixOrder()
+        {
+            return _data.SelectMany(x => x);
+        }
+    }
+}
