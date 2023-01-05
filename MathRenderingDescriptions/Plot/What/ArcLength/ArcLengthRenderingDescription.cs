7402b495f4876c924e6a73d3948c61be385eba24
Scott Haney <emailalmondbranch@gmail.com>
2023-01-05 17:41:06 -0500

    Start filling in the arc length rendering description

--- MathRenderingDescriptions/Plot/What/ArcLength/ArcLengthRenderingDescription.cs
+++ MathRenderingDescriptions/Plot/What/ArcLength/ArcLengthRenderingDescription.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using MathRenderingDescriptions.Plot.When;
+using RenderingDescriptions.What;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MathRenderingDescriptions.Plot.What.ArcLength
 {
-    public class ArcLengthRenderingDescription
+    public class ArcLengthRenderingDescription : IWhatToRender
     {
+        public readonly FunctionRenderingDescription FunctionDescription;
+        public readonly FunctionRenderingDescription DerivativeDescription;
+        public readonly ITimingDescription TimingDescription;
+
+        public ArcLengthRenderingDescription(string uniqueName,
+            FunctionRenderingDescription functionDescription,
+            FunctionRenderingDescription derivativeDescription,
+            ITimingDescription timingDescription)
+            : base(uniqueName)
+        {
+            FunctionDescription = functionDescription;
+            DerivativeDescription = derivativeDescription;
+            TimingDescription = timingDescription;
+        }
     }
 }
