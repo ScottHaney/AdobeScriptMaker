6b254ea6690ca0b6ed3b66b202d3f1ee2abd4177
Scott Haney <emailalmondbranch@gmail.com>
2023-01-05 17:35:46 -0500

    Start work on the arc length renderer

---
+++ MathRenderingDescriptions/Plot/What/ArcLength/ArcLengthRenderingDescription.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathRenderingDescriptions.Plot.What.ArcLength
+{
+    public class ArcLengthRenderingDescription
+    {
+    }
+}
