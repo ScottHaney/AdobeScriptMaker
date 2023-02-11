cc853f95074ec4d2ac8762cf64283a8f7928bcf3
Scott Haney <emailalmondbranch@gmail.com>
2023-02-10 21:56:22 -0500

    Get the script to render something that resembles the digit 0

--- IllustratorRenderingDescriptions.Tests/DigitSculptureTests.cs
+++ IllustratorRenderingDescriptions.Tests/DigitSculptureTests.cs
@@ -11,7 +11,13 @@ namespace IllustratorRenderingDescriptions.Tests
         {
             var digitBoundingBox = new RectangleF(0, 0, 500, 800);
             var sculpture = new DigitSculpture(digitBoundingBox,
-                new DigitCorner(DigitCornerName.TopLeft, 0.1f, 45));
+                new DigitCorner(DigitCornerName.TopLeft, 0.1f, 45),
+                new DigitCorner(DigitCornerName.TopRight, 0.1f, 45),
+                new DigitCorner(DigitCornerName.BottomRight, 0.1f, 45),
+                new DigitCorner(DigitCornerName.BottomLeft, 0.1f, 45),
+                new DigitHole(DigitHoleName.Top, 0.2f),
+                new DigitHole(DigitHoleName.Bottom, 0.2f),
+                new DigitCrossBar(0.2f));
 
             var script = sculpture.Carve();
         }
