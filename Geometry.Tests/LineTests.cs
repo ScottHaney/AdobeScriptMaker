f1028c173ca8041406c099201616e9e3f0ef74f4
Scott Haney <emailalmondbranch@gmail.com>
2023-02-16 08:00:40 -0500

    Add in parametric line tests for non vertical lines

--- Geometry.Tests/LineTests.cs
+++ Geometry.Tests/LineTests.cs
@@ -28,5 +28,32 @@ namespace Geometry.Tests
 
             Assert.AreEqual(1, line.GetSlope());
         }
+
+        [Test]
+        public void The_Parametric_Value_Of_A_Point_Not_On_A_Non_Vertical_Line_Is_Null()
+        {
+            var line = new Line(new PointF(0, 0), new PointF(1, 1));
+            var actualResult = line.GetParametericValue(new PointF(0, 5));
+
+            Assert.AreEqual(null, actualResult);
+        }
+
+        [Test]
+        public void The_Parametric_Value_Of_A_Point_On_A_Non_Vertical_Line_To_The_Right_Of_The_Start_Point_Is_The_X_Difference_From_The_Start_Point()
+        {
+            var line = new Line(new PointF(0, 0), new PointF(1, 1));
+            var actualResult = line.GetParametericValue(new PointF(2, 2));
+
+            Assert.AreEqual(2, actualResult);
+        }
+
+        [Test]
+        public void The_Parametric_Value_Of_A_Point_On_A_Non_Vertical_Line_To_The_Left_Of_The_Start_Point_Is_The_X_Difference_From_The_Start_Point()
+        {
+            var line = new Line(new PointF(0, 0), new PointF(1, 1));
+            var actualResult = line.GetParametericValue(new PointF(-1, -1));
+
+            Assert.AreEqual(-1, actualResult);
+        }
     }
 }
