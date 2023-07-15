f596a357643e7dfd63cdb5d8bc44b7c9e2c22b31
Scott Haney <emailalmondbranch@gmail.com>
2023-07-15 15:45:30 -0400

    Clean up the design time data

---
+++ AdobeScriptMaker.UI.ViewModels/DesignTimeData/DesignTimeScriptBuilderViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AdobeScriptMaker.UI.ViewModels.ScriptBuilder;
+using AdobeScriptMaker.UI.ViewModels.ScriptBuilder.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdobeScriptMaker.UI.ViewModels.DesignTimeData
+{
+    public class DesignTimeScriptBuilderViewModel
+    {
+        public List<ScriptBuilderComponentViewModel> Components { get; set; }
+
+        public DesignTimeScriptBuilderViewModel()
+        {
+            Components = new List<ScriptBuilderComponentViewModel>();
+
+            var component = new ScriptBuilderComponentViewModel();
+            component.Name = "Test Component";
+
+            Components.Add(component);
+        }
+    }
+}
--- AdobeScriptMaker.UI/App.xaml.cs
+++ AdobeScriptMaker.UI/App.xaml.cs
@@ -14,20 +14,5 @@ namespace AdobeScriptMaker.UI
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnActivated(EventArgs e)
-        {
-            base.OnActivated(e);
-
-            MainWindow.DataContext = new ScriptBuilderViewModel()
-            {
-                Components = new List<ScriptBuilderComponentViewModel> {
-                    new ScriptBuilderComponentViewModel()
-                    {
-                        Name = "Test Component",
-                        Parameters = new List<ViewModels.ScriptBuilder.Parameters.IScriptBuilderParameter>()
-                    }
-                }
-            };
-        }
     }
 }
