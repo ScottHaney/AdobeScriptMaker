7cd6dc834c41a73a67802ea1d80a96ffc09bcc4a
Scott Haney <scotthaney323@gmail.com>
2023-07-22 17:45:28 -0400

    Add in the updated repositioning code

--- AdobeScriptMaker.UI.ViewModels/Timeline/TimelineViewModel.cs
+++ AdobeScriptMaker.UI.ViewModels/Timeline/TimelineViewModel.cs
@@ -30,38 +30,101 @@ namespace AdobeScriptMaker.UI.Core.Timeline
         {
             if (message.Direction == ResizeDirection.End)
             {
-                var index = components.IndexOf(message.Component);
-
-                double maxValue;
-                if (index + 1 < components.Count)
-                    maxValue = components[index + 1].Start;
-                else
-                    maxValue = width;
-
-                var updatedValue = message.Component.End + message.SizeChange;
-                if (updatedValue <= maxValue && updatedValue >= message.Component.Start)
-                    message.Component.End += message.SizeChange;
+                var endBounds = GetEndMovementBounds(message.Component);
+                message.Component.End = endBounds.ClampedValue(message.Component.End + message.SizeChange);
             }
             else if (message.Direction == ResizeDirection.Start)
             {
-                var index = components.IndexOf(message.Component);
-
-                double minValue;
-                if (index - 1 >= 0)
-                    minValue = components[index - 1].End;
-                else
-                    minValue = 0;
-
-                var updatedValue = message.Component.Start + message.SizeChange;
-                if (updatedValue >= minValue && updatedValue <= message.Component.End)
-                    message.Component.Start += message.SizeChange;
+                var startBounds = GetStartMovementBounds(message.Component);
+                message.Component.Start = startBounds.ClampedValue(message.Component.Start + message.SizeChange);
             }
         }
 
         public void Receive(RepositionTimelineComponentMessage message)
         {
-            message.Component.Start += message.PositionChange;
-            message.Component.End += message.PositionChange;
+            var startBounds = GetStartMovementBounds(message.Component);
+            var usedStartDist = startBounds.GetUsedDistance(message.Component.Start, message.PositionChange);
+
+            var endBounds = GetEndMovementBounds(message.Component);
+            var usedEndDist = endBounds.GetUsedDistance(message.Component.End, message.PositionChange);
+
+            var minDist = Math.Min(usedStartDist, usedEndDist);
+            var updatedChange = Math.Sign(message.PositionChange) * minDist;
+
+            message.Component.Start = message.Component.Start + updatedChange;
+            message.Component.End = message.Component.End + updatedChange;
+        }
+
+        private MovementBounds GetEndMovementBounds(TimelineComponentViewModel component)
+        {
+            return new MovementBounds(component.Start, GetEndMax(component));
+        }
+
+        private MovementBounds GetStartMovementBounds(TimelineComponentViewModel component)
+        {
+            return new MovementBounds(GetStartMin(component), component.End);
+        }
+
+        private double GetEndMax(TimelineComponentViewModel component)
+        {
+            var index = components.IndexOf(component);
+            if (index == components.Count - 1)
+                return width;
+            else
+            {
+                var nextComponent = components[index + 1];
+                return nextComponent.Start;
+            }
+        }
+
+        private double GetStartMin(TimelineComponentViewModel component)
+        {
+            var index = components.IndexOf(component);
+            if (index == 0)
+                return 0;
+            else
+            {
+                var previousComponent = components[index - 1];
+                return previousComponent.End;
+            }
+        }
+
+        private class MovementBounds
+        {
+            private readonly double _min;
+            private readonly double _max;
+
+            public MovementBounds(double min, double max)
+            {
+                _min = min;
+                _max = max;
+            }
+
+            public bool IsInBounds(double value)
+            {
+                return _min <= value && value <= _max;
+            }
+
+            public double ClampedValue(double value)
+            {
+                if (value < _min)
+                    return _min;
+                else if (value > _max)
+                    return _max;
+                else
+                    return value;
+            }
+
+            public double GetUsedDistance(double originalValue, double additionalValue)
+            {
+                var updatedValue = originalValue + additionalValue;
+                if (updatedValue < _min)
+                    return originalValue - _min;
+                else if (updatedValue > _max)
+                    return _max - originalValue;
+                else
+                    return Math.Abs(additionalValue);
+            }
         }
     }
 }
