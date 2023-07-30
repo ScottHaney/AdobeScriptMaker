b33598791877853e943c9faffd640e97a5429f05
Scott Haney <emailalmondbranch@gmail.com>
2023-07-29 20:44:51 -0400

    Move the starting numeric parameters into a template selector

---
+++ AdobeScriptMaker.UI.ViewModels/PropertiesEditor/PropertiesEditorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdobeScriptMaker.UI.Core.PropertiesEditor
+{
+    public partial class PropertiesEditorViewModel : ObservableObject
+    {
+
+    }
+}
---
+++ AdobeScriptMaker.UI/Views/PropertiesEditor/PropertyItemTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AdobeScriptMaker.UI.Views.PropertiesEditor
+{
+    public class PropertyItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DoubleTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item,
+            DependencyObject container)
+        {
+            return DoubleTemplate;
+        }
+    }
+}
