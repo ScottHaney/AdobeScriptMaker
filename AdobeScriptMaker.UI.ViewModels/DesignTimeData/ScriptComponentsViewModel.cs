b7f6f9326198b59740022c25d84b21632765f4a8
Scott Haney <emailalmondbranch@gmail.com>
2023-08-06 18:03:35 -0400

    Add in some design time data for the script component

---
+++ AdobeScriptMaker.UI.ViewModels/DesignTimeData/ScriptComponentsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using AdobeScriptMaker.UI.Core.DataModels;
+using AdobeScriptMaker.UI.Core.ScriptBuilder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdobeScriptMaker.UI.Core.DesignTimeData
+{
+    public class ScriptComponentsViewModel
+    {
+        public List<ScriptComponentViewModel> Components { get; set; } = new List<ScriptComponentViewModel>();
+
+        public string Name { get; set; }
+
+        public IScriptComponentDataModel ComponentData { get; set; }
+
+        public IEnumerable<IScriptComponentDataModel> SamplePrimitives { get; set; }
+
+        public ScriptComponentsViewModel()
+        {
+            Components.Add(new ScriptComponentViewModel()
+            {
+                Name = "Plot Axes",
+                ComponentData = new AxesDataModel(),
+                SamplePrimitives = new[]
+                {
+                    new AxesDataModel()
+                }
+            });
+        }
+    }
+
+    public class ScriptComponentViewModel
+    {
+        public string Name { get; set; }
+
+        public IScriptComponentDataModel ComponentData { get; set; }
+
+        public IEnumerable<IScriptComponentDataModel> SamplePrimitives { get; set; }
+    }
+}
