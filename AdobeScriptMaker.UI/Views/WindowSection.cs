825997a43fc1c693153f972e866849b33e790775
Scott Haney <emailalmondbranch@gmail.com>
2023-08-13 15:30:30 -0400

    Create boundaries for each of the window sections

---
+++ AdobeScriptMaker.UI/Views/WindowSection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdobeScriptMaker.UI.Views
+{
+    public class WindowSection : ContentControl
+    {
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(WindowSection), new PropertyMetadata(null));
+
+        public DataTemplateSelector HeaderTemplateSelector
+        {
+            get { return (DataTemplateSelector)GetValue(HeaderTemplateSelectorProperty); }
+            set { SetValue(HeaderTemplateSelectorProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderTemplateSelectorProperty =
+            DependencyProperty.Register("HeaderTemplateSelector", typeof(DataTemplateSelector), typeof(WindowSection), new PropertyMetadata(null));
+
+        public string HeaderStringFormat
+        {
+            get { return (string)GetValue(HeaderStringFormatProperty); }
+            set { SetValue(HeaderStringFormatProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderStringFormatProperty =
+            DependencyProperty.Register("HeaderStringFormat", typeof(string), typeof(WindowSection), new PropertyMetadata(null));
+
+        public Brush BorderBrush
+        {
+            get { return (Brush)GetValue(BorderBrushProperty); }
+            set { SetValue(BorderBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty BorderBrushProperty =
+            DependencyProperty.Register("BorderBrush", typeof(Brush), typeof(WindowSection), new PropertyMetadata(null));
+
+        public Thickness BorderThickness
+        {
+            get { return (Thickness)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty BorderThicknessProperty =
+            DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(WindowSection), new PropertyMetadata(new Thickness()));
+
+        public DataTemplate HeaderTemplate
+        {
+            get { return (DataTemplate)GetValue(HeaderTemplateProperty); }
+            set { SetValue(HeaderTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderTemplateProperty =
+            DependencyProperty.Register("HeaderTemplate", typeof(DataTemplate), typeof(WindowSection), new PropertyMetadata(null));
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(WindowSection), new PropertyMetadata(new CornerRadius(4)));
+
+        static WindowSection()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowSection), new FrameworkPropertyMetadata(typeof(WindowSection)));
+        }
+    }
+}
