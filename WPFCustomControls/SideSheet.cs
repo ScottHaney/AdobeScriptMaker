694056b0fda09e72c536b16deba722608b2aae44
Scott Haney <scotthaney323@gmail.com>
2023-08-19 15:01:31 -0400

    Bind the IsActive property on the SideSheet to the Visibility property

--- WPFCustomControls/SideSheet.cs
+++ WPFCustomControls/SideSheet.cs
@@ -26,36 +26,11 @@ namespace WPFCustomControls
         }
 
         public static readonly DependencyProperty IsActiveProperty =
-            DependencyProperty.Register("IsActive", typeof(bool), typeof(SideSheet), new PropertyMetadata(false, IsActiveChanged));
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(SideSheet), new PropertyMetadata(false));
 
         static SideSheet()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(SideSheet), new FrameworkPropertyMetadata(typeof(SideSheet)));
-        }
-
-        public SideSheet()
-        {
-            Loaded += SideSheet_Loaded;
-        }
-
-        private void SideSheet_Loaded(object sender, RoutedEventArgs e)
-        {
-            RenderTransform = new TranslateTransform(ActualWidth, 0);
-        }
-
-        private static void IsActiveChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
-        {
-            var sideSheet = (SideSheet)o;
-            var isActive = (bool)args.NewValue;
-
-            sideSheet.RenderTransform = new TranslateTransform(isActive ? 0 : sideSheet.ActualWidth, 0);
-
-            /*sideSheet.SetValue(WidthProperty, DependencyProperty.UnsetValue);
-
-            var targetValue = isActive ? sideSheet.DesiredSize.Width : 0;
-
-            var animation = new DoubleAnimation() { To = targetValue, Duration = new Duration(TimeSpan.FromSeconds(2)) };
-            sideSheet.BeginAnimation(WidthProperty, animation);*/
         }
     }
 }
