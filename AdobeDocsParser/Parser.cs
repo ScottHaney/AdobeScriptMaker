bb258305607a0727e01e51d4d96ee3000bb0e022
Scott Haney <emailalmondbranch@gmail.com>
2023-12-01 21:03:39 -0800

    Start some projects to read in the after effects scripting docs

---
+++ AdobeDocsParser/Parser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig;
+using System.Linq;
+
+namespace AdobeDocsParser;
+
+public class Parser(string _pdfFilePath)
+{
+    public void Test()
+    {
+        using var document = PdfDocument.Open(_pdfFilePath);
+
+        foreach (Page page in document.GetPages())
+        {
+            var letters = page.Letters;
+            var example = string.Join(string.Empty, letters.Select(x => x.Value));
+
+            var words = page.GetWords();
+
+            var images = page.GetImages();
+        }
+    }
+}
---
+++ AdobeDocsRunner/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+using AdobeDocsParser;
+
+var parser = new Parser(@"\\Mac\Home\Desktop\ae-scripting-docsforadobe-dev-en-latest.pdf");
+parser.Test();
